fdd898010422ed7d5cbba759abf20159718755eb
Boukind Rachid <boukind.r1@gmail.com>
2023-10-18 06:47:23 -0700

    O

--- tokenizer.c
+++ tokenizer.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * Tokenize- a string into an array of strings using a specified delimiter.
+ * tokenizer- a string into an array of strings using a specified delimiter.
  * @line: The input string to be tokenized.
  * Return: An array of strings obtained by splitting the input string.
  *         Returns NULL if memory allocation or tokenization fails.
@@ -9,43 +9,31 @@
 
 char **tokenizer(char *line)
 {
-	char *token = NULL, *tmp = NULL;
-	char **command = NULL;
+	char *token = NULL, *tmp = NULL, **command = NULL;
 	int cpt = 0, i = 0, j;
 
 	if (!line)
 		return (NULL);
-
 	tmp = _strdup(line);
 	if (!tmp)
 	{
-		free(line);
-		return (NULL);
+		free(line), return (NULL);
 	}
-
 	token = strtok(tmp, DELIM);
 	while (token)
 	{
-		cpt++;
-		token = strtok(NULL, DELIM);
+		cpt++, token = strtok(NULL, DELIM);
 	}
-	free(tmp);
-
-	command = malloc(sizeof(char *) * (cpt + 1));
+	free(tmp), command = malloc(sizeof(char *) * (cpt + 1));
 	if (!command)
 	{
-		free(line);
-		return (NULL);
+		free(line), return (NULL);
 	}
-
 	tmp = _strdup(line);
 	if (!tmp)
 	{
-		free(line);
-		free(command);
-		return (NULL);
+		free(line), free(command), return (NULL);
 	}
-
 	token = strtok(tmp, DELIM);
 	while (token)
 	{
@@ -56,16 +44,9 @@ char **tokenizer(char *line)
 			{
 				free(command[j]);
 			}
-			free(command);
-			free(line);
-			return (NULL);
+			free(command), free(line), return (NULL);
 		}
-		token = strtok(NULL, DELIM);
-		i++;
+		token = strtok(NULL, DELIM), i++;
 	}
-	free(tmp);
-	free(line);
-	command[i] = NULL;
-	return (command);
+	free(tmp), free(line), command[i] = NULL, return (command);
 }
-
