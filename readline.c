540021e07eed347617b8ad682e2822e61a086dad
Boukind Rachid <boukind.r1@gmail.com>
2023-10-17 20:04:24 -0700

    K

--- readline.c
+++ readline.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+/**
+ * read_line- Read a line of input from the user and allocate memory for it.
+ *
+ * Return: A dynamically allocated string containing the user's input line.
+ *         Returns NULL if memory allocation or input read fails.
+*/
 
 char *read_line(void)
 {
@@ -7,15 +13,15 @@ char *read_line(void)
 	size_t len = 0;
 	ssize_t n;
 
-	if(isatty(STDOUT_FILENO))
+	if (isatty(STDOUT_FILENO))
 		write(STDOUT_FILENO, "$", 2);
 
 	n = getline(&line, &len, stdin);
-	
-	if( n == -1 )
+
+	if (n == -1)
 	{
 		free(line);
-		return(NULL);
+		return (NULL);
 	}
 
 	return (line);
